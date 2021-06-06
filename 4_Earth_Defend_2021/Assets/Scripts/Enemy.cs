d5fe5fe86fd7c431e2113068db61c42bd3f92475
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2021-06-06 21:47:43 +0530

    8th Commit
    New Scene called spaceship created where customize our ships. Score system.

--- 4_Earth_Defend_2021/Assets/Scripts/Enemy.cs
+++ 4_Earth_Defend_2021/Assets/Scripts/Enemy.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject EnemyDestroyVFX;
+    [SerializeField] GameObject enemyDestroyVFX;
+    [SerializeField] GameObject enemyHitVFX;
     [SerializeField] Transform parent;
+
+    ScoreBoard scoreBoard;
+
+    [Tooltip("Score for scoreBoard when player destroys a enemy.")] [SerializeField] int enemyScore;
+
+    [SerializeField] int hitPoint = 2;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        gameObject.AddComponent<Rigidbody>().useGravity = false;
+    }
 
     void OnParticleCollision(GameObject other)
     {
-        GameObject vfx = Instantiate(EnemyDestroyVFX, transform.position, Quaternion.identity);
+        ProcessHit();
+        if(hitPoint < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        HitVfx();
+        hitPoint--;
+        scoreBoard.IncreaseScore(enemyScore);
+    }
+
+    void HitVfx()
+    {
+        GameObject vfx = Instantiate(enemyHitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+    }
+
+    void KillEnemy()
+    {
+        GameObject vfx = Instantiate(enemyDestroyVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(this.gameObject);
     }
-
+    
 }
